c334e531b325f09d595cff8448ec5aacc62752a1
N2K-keli <kelinguma@gmail.com>
2026-01-29 17:58:42 +0100

    Initial commit: Visual Studio SFML project

---
+++ TTT/TTT.cpp
@@ -0,0 +1,23 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(
+        sf::VideoMode({ 800, 600 }),
+        "SFML 3 - Visual Studio"
+    );
+
+    while (window.isOpen())
+    {
+        while (auto event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.clear();
+        window.display();
+    }
+
+    return 0;
+}
